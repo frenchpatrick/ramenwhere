51d3a3c60fdc0d8f9282ccf3ff727e98de2d36b4
Pat French <pat.mfrench@yahoo.com>
2023-12-23 19:51:01 +0000

    Update

--- Hello.py
+++ Hello.py
@@ -21,8 +21,6 @@ import altair as alt
 import os
 from pandas import json_normalize
 
-api_key = os.environ.get("YELP_API_KEY")
-
 #API request
 url = "https://api.yelp.com/v3/businesses/search?location=los%20angeles&term=ramen&sort_by=review_count&limit=50"
 headers = {
